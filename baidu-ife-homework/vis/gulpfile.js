d75861869aa8a2a51501ecb911a8414454b2f39c
wanglei <fatelei@gmail.com>
2015-08-16 00:36:06 +0800

    second commit

--- baidu-ife-homework/vis/gulpfile.js
+++ baidu-ife-homework/vis/gulpfile.js
@@ -1,5 +1,26 @@
 var gulp = require("gulp");
+var less = require("gulp-less");
+var path = require("path");
+var connect = require('gulp-connect');
 
-gulp.task("default", function () {
-
+gulp.task("less", function () {
+  gulp.src("./task1/less/*.less")
+    .pipe(less({
+      paths: [path.join(__dirname, "less")]
+    }))
+    .pipe(gulp.dest("./task1/css"));
 });
+
+gulp.task("connect", function () {
+  connect.server({
+    root: "task1",
+    livereload: true,
+    port: 8000
+  });
+});
+
+gulp.task("watch", function () {
+  gulp.watch(["./task1/less/*.less"], ["less"]);
+})
+
+gulp.task("default", ["less", "connect", "watch"]);
